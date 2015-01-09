bdbff215a37434f566a0ea77dd0bf7e028bc29f5
Amy Michelle Johnson <amy.johnson.michelle@gmail.com>
2015-01-09 14:28:49 -0800

    Make minor changes in indentation and spacing for cleaner looking code

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,7 +1,7 @@
 class String
   define_method (:word_frequency) do |argument|
     n = 0
-    (split()).each() do |word|
+    split().each() do |word|
       if word.downcase() == argument.downcase()
         n = n + 1
       end
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -5,9 +5,8 @@ describe ("String#word_frequency") do
   it ("counts the number of times the inputed word is contained in a one-word string") do
     expect("cat".word_frequency("cat")).to(eq(1))
   end
-
   it ("counts the number of times the inputed word is contained in a string of words") do
-    expect("My cat is a very good cat".word_frequency("cat")).to(eq(2))
+    expect("my cat is a very good cat".word_frequency("cat")).to(eq(2))
   end
   it ("counts the number of times the inputed word is contained in a string of words regardless of capitalization") do
     expect("The cat is the best".word_frequency("the")).to(eq(2))
